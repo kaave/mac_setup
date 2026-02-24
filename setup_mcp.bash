21ec14e0e25544987bb546a97d2f54dd3f9c6232
kaave <junkjunctions@gmail.com>
2026-02-24 09:39:26 +0900

    add: codex mcp

--- setup_mcp.bash
+++ setup_mcp.bash
@@ -5,6 +5,7 @@ if command -v claude >/dev/null 2>&1; then
   claude mcp add --scope user --transport http context7 https://mcp.context7.com/mcp --header "CONTEXT7_API_KEY: \${__KEY_CONTEXT7}"
   claude mcp add --scope user --transport http atlassian https://mcp.atlassian.com/v1/mcp
   claude mcp add --scope user --transport http slack https://mcp.slack.com/mcp
+  claude mcp add --scope user codex codex mcp-server
   claude mcp add --scope user --env NOTION_TOKEN=$__KEY_NOTION notion -- npx -y @notionhq/notion-mcp-server
 fi
 
